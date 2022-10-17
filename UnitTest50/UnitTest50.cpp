31b9c5f098f3e541f8fbbc3d668e925ebb1c7c9b
Qunilche <orest_teg@ukr.net>
2022-10-17 07:15:05 +0300

    Add project files.

---
+++ UnitTest50/UnitTest50.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest50
+{
+	TEST_CLASS(UnitTest50)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
